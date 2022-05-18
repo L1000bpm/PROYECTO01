17aa06fbbee16b339b5d3449e83f093b0a017cb1
leonardo fuentes <leitoleon7232@gmail.com>
2022-05-18 12:52:35 -0500

    variables y constantes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
 fun main(){
-    print("hello");
+
+    var nombre : String = "Leonardo"
+    var apellido : String = "Fuentes"
+
+    println("Me llamo ${nombre.uppercase()} ${apellido.lowercase()}")
+
+    val dia : Int = 1
+    val mes : Int = 7
+    
+    println("Mi Cumpleaños es: $dia/$mes")
+
 }
